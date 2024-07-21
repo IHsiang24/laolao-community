db5c84356ce06175a0841fdc771f573fa692c0aa
xiangkai <2939059539@qq.com>
2024-07-21 23:59:31 +0800

    LoginRequiredInterceptor判断handler类型，防止抛ClassCastException异常

--- src/main/java/com/xiangkai/community/config/interceptor/LoginRequiredInterceptor.java
+++ src/main/java/com/xiangkai/community/config/interceptor/LoginRequiredInterceptor.java
@@ -19,14 +19,16 @@ public class LoginRequiredInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        HandlerMethod method = (HandlerMethod) handler;
-        LoginRequired loginRequired = method.getMethodAnnotation(LoginRequired.class);
-        User user = hostHolder.get();
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod method = (HandlerMethod) handler;
+            LoginRequired loginRequired = method.getMethodAnnotation(LoginRequired.class);
+            User user = hostHolder.get();
 
-        // 如果用户未登录，则禁止访问某些页面，直接跳转至登录页
-        if (loginRequired != null && user == null) {
-            response.sendRedirect(request.getContextPath() + "/login");
-            return false;
+            // 如果用户未登录，则禁止访问某些页面，直接跳转至登录页
+            if (loginRequired != null && user == null) {
+                response.sendRedirect(request.getContextPath() + "/login");
+                return false;
+            }
         }
 
         return true;
