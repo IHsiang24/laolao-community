53a223d9e607ff8f0971c85934145c33d4007323
xiangkai <2939059539@qq.com>
2024-07-22 15:58:02 +0800

    增加aop记录日志

---
+++ src/main/java/com/xiangkai/community/aspect/LoggerAspect.java
@@ -0,0 +1,42 @@
+package com.xiangkai.community.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Aspect
+public class LoggerAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggerAspect.class);
+
+    @Pointcut("execution(* com.xiangkai.community.service.*.*(..))")
+    public void pointcut() {
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        Signature signature = joinPoint.getSignature();
+        String beanName = signature.getDeclaringTypeName();
+        String methodName = signature.getName();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date = dateFormat.format(new Date());
+        LOGGER.info(String.format("用户[%s], 于[%s]访问了[%s].", ip, date, beanName + "." + methodName));
+    }
+
+}
