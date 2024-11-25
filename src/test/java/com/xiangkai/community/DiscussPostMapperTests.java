abf600103d552c4b688f71ff9ca8e099a2492dc9
xiangkai <2939059539@qq.com>
2024-11-26 00:34:31 +0800

    新增测试MyBatis缓存功能的测试用例

--- src/test/java/com/xiangkai/community/DiscussPostMapperTests.java
+++ src/test/java/com/xiangkai/community/DiscussPostMapperTests.java
@@ -1,6 +1,9 @@
 package com.xiangkai.community;
 
 import com.xiangkai.community.dao.mapper.DiscussPostMapper;
+import com.xiangkai.community.model.entity.DiscussPost;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +16,16 @@ public class DiscussPostMapperTests {
 
     @Test
     void selectDiscussPosts() {
+        SqlSession sqlSession = null;
+        sqlSession.clearCache();
+        long start1 = System.currentTimeMillis();
         System.out.println(discussPostMapper.selectDiscussPosts(101, 0, 100, 0));
+        System.out.println("首次查询耗时：" + (System.currentTimeMillis() - start1)/1000 + "秒");
+
+        long start2 = System.currentTimeMillis();
+        System.out.println(discussPostMapper.selectDiscussPosts(101, 0, 100, 0));
+        System.out.println("第二次查询耗时：" + (System.currentTimeMillis() - start2)/1000 + "秒");
+
     }
 
     @Test
